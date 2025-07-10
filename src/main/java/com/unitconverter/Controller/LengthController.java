34a95ffd28fea4224756d07a0d1fe7c5ae9720f3
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-10 17:54:22 -0500

    Registrando convert

--- src/main/java/com/unitconverter/Controller/LengthController.java
+++ src/main/java/com/unitconverter/Controller/LengthController.java
@@ -5,23 +5,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
 @Controller
 @RequestMapping("/length")
+@Slf4j
 public class LengthController {
     @GetMapping
     public String index() {
+        log.info("RETORNANDO VISTA");
+        System.out.println("RETORNANDO VISTA");
         return "index";
     }
 
-    @PostMapping
+    @PostMapping("/convert")
     public String convertToLength(@RequestParam("unit") double unit, @RequestParam("from") String from, @RequestParam("to") String to){
-        
-        return "";
+        System.out.println("RETORNANDO VISTA");
+        log.info("---------------------------Numero a convertir es: "+unit+" de "+from+" a "+to);
+        return "index";
     }
     
 }
