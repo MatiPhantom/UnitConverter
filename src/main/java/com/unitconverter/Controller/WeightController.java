8c6ceed62a5c14d78c16157e25777b0c0bf0f9c0
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-11 17:34:03 -0500

    Creacion de service

--- src/main/java/com/unitconverter/Controller/WeightController.java
+++ src/main/java/com/unitconverter/Controller/WeightController.java
@@ -1,5 +1,25 @@
 package com.unitconverter.Controller;
 
-public class WeightController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.unitconverter.Service.WeightService;
+
+
+@Controller
+@RequestMapping("/weight")
+public class WeightController {
+    
+    @Autowired
+    private WeightService weightService;
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("unities", weightService.getUnities());
+        return "weight/index";
+    }
+    
 }
